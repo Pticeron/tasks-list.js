c3631a52749b0b2ff87ad726a7f512a44ffa3018
Inna Zorina <innazorina0101@gmail.com>
2023-04-01 14:28:33 +0300

    commit

--- src/js/01-todo-list.js
+++ src/js/01-todo-list.js
@@ -30,13 +30,17 @@ class TodoList {
   }
 
 
-#defineRefs() {
-  this.#refs.app = document.querySelector(`.todo-list`);
-  if (this.#refs.app) {
-    this.#refs.addItemBtn = this.#refs.app.querySelector(`.header__button`);
-    this.#refs.itemInput = this.#refs.app.querySelector(`.header__input`);
-}
-}
+  #defineRefs() {
+    this.#refs.app = document.querySelector('.todo-list');
+    if (this.#refs.app) {
+      this.#refs.addItemBtn = this.#refs.app.querySelector('.header__button');
+      this.#refs.itemInput = this.#refs.app.querySelector('.header__input');
+      this.#refs.currentList = this.#refs.app.querySelector('.current-list');
+      this.#refs.doneList = this.#refs.app.querySelector('.done-list');
+      this.#refs.listContainer = document.querySelector('.list-container');
+    }
+  }
+
 
 #initListeners() {
   this.#refs.addItemBtn.addEventListener(`click`, this.#addTask.bind(this));
@@ -46,10 +50,34 @@ class TodoList {
 #addTask() {
   const { value } = this.#refs.itemInput;
 
-    if (value) {
-      this.#items.push({ id: nanoid(), value, done: false });
-      console.log(this.#items);
+  if (value) {
+    const items = [...this.#items];
+    items.push({ id: nanoid(), value, done: false });
+
+    this.#updateItems(items);
   }
+
+  this.#refs.itemInput.value = null;
+}
+
+#render() {
+  const getItem = ({id, value, done}) =>
+  `
+  <li class="list__item" data-id="${id}" data-done="${done}">
+    <span class="list__item-name">${value}</span>
+    <div class="list__buttons">
+      <i class="list__button remove fa fa-trash-o" data-action="remove"></i>
+      <span class="list__bar">|</span>
+      <i 
+        class="${`list__button fa ${done ? 'uncomplete fa-check-circle' : 'complete fa-check-circle-o'}`}"
+        data-action="toggle"
+      ></i>
+    </div>
+  </li>
+`;
+
+const todoTasks = this.#items.filter(({ done }) => !done);
+const doneTasks = this.#items.filter(({ done }) => done);
 }
 }
   
