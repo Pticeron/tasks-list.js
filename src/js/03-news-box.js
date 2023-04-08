9f4c9679b867058a3721dc76f86904962b9cd43d
Inna Zorina <innazorina0101@gmail.com>
2023-04-08 10:59:40 +0300

    add new-box

---
+++ src/js/03-news-box.js
@@ -0,0 +1,32 @@
+const markup = `
+    <section class="news-app">
+    <section class="header">
+        <div class='header__wrapper'>
+            <a class='header__logo'>
+                <i class="fa-regular fa-newspaper"></i>
+            </a>
+            <div class='header__brand'>News Box</div>
+        </div>
+    </section>
+    <form class="search">
+        <div class="search__wrapper">
+            <input class="search__field" type="text" placeholder="What are you looking for?" name="search">
+            <button class="search__btn" type="submit">
+            <span class="search__icon">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <i class="fa-solid fa-circle-notch"></i>
+            </span>
+            </button>
+        </div>
+    </form>
+    <div class="articles"></article> 
+    </div>
+    <section class="load-more">
+    <button class="load-more__btn" hidden>More</button>
+    </section>
+    </section>
+    `;
+
+    // add to DOM
+
+document.body.innerHTML = markup;
